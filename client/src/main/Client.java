bd84c24ced9578afcd01278cb5911b28c6e09663
dmueller <david.mllr@web.de>
2019-04-17 13:53:42 +0200

    basic client + icon added

---
+++ client/src/main/Client.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+import lenz.htw.sawhian.Move;
+import lenz.htw.sawhian.net.NetworkClient;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage logo = ImageIO.read(new File("client/resources/circle.png"));
+        NetworkClient client = new NetworkClient(null, "CLIENT -1", logo);
+
+        client.getMyPlayerNumber();
+        client.getTimeLimitInSeconds();
+        client.getExpectedNetworkLatencyInMilliseconds();
+
+        while (true) {
+            Move move = client.receiveMove();
+            if (move == null) {
+                // move = meinTotalClevererZug();
+                //uncleverer Zug: new Move(client.getMyPlayerNumber(),0,0);
+                client.sendMove(move);
+            } else {
+                // moveInMeinSpielfeldIntegrieren(move);
+            }
+        }
+    }
+}
